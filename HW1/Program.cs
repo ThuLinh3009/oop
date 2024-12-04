2e1d32c683c9c28b04e19fe39c6c49521f72b726
ThuLinh3009 <linhdoanarmy3009@gmail.com>
2024-12-04 23:40:22 +0700

    first commit

---
+++ HW1/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(" Doan Thi Thu Linh\t12423020\t12423TN\tThuLinh3009\tlinhdoanarmy3009@gmail.com");
+        Console.ReadKey();
+    }
+}
